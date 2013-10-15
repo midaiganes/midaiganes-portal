425ecd0838286e5fabd9c41175b177f699b30e36
midaiganes <midaiganes@panic>
2013-10-15 09:09:50 +0300

    refactor

--- portal-service/src/main/java/ee/midaiganes/servlet/PortalServlet.java
+++ portal-service/src/main/java/ee/midaiganes/servlet/PortalServlet.java
@@ -30,143 +30,140 @@ import ee.midaiganes.services.exceptions.DbInstallFailedException;
 import ee.midaiganes.services.exceptions.PrincipalException;
 import ee.midaiganes.servlet.http.ThemeServletRequest;
 import ee.midaiganes.util.ContextUtil;
-import ee.midaiganes.util.PortalUtil;
 import ee.midaiganes.util.RequestUtil;
 import ee.midaiganes.util.StringPool;
 
 public class PortalServlet extends HttpServlet {
-	private static final Logger log = LoggerFactory.getLogger(PortalServlet.class);
-	private static final String WEBINF_THEME = "/WEB-INF/theme";
+    private static final Logger log = LoggerFactory.getLogger(PortalServlet.class);
+    private static final String WEBINF_THEME = "/WEB-INF/theme";
 
-	@Resource(name = PortalConfig.DB_INSTALL_SERVICE)
-	private DbInstallService dbInstallService;
+    @Resource(name = PortalConfig.DB_INSTALL_SERVICE)
+    private DbInstallService dbInstallService;
 
-	@Resource(name = PortalConfig.LAYOUT_PORTLET_REPOSITORY)
-	private LayoutPortletRepository layoutPortletRepository;
+    @Resource(name = PortalConfig.LAYOUT_PORTLET_REPOSITORY)
+    private LayoutPortletRepository layoutPortletRepository;
 
-	@Resource(name = PortalConfig.SECURE_PORTLET_REPOSITORY)
-	private SecurePortletRepository portletRepository;
+    @Resource(name = PortalConfig.SECURE_PORTLET_REPOSITORY)
+    private SecurePortletRepository portletRepository;
 
-	@Resource(name = PortalConfig.PORTLET_INSTANCE_REPOSITORY)
-	private PortletInstanceRepository portletInstanceRepository;
+    @Resource(name = PortalConfig.PORTLET_INSTANCE_REPOSITORY)
+    private PortletInstanceRepository portletInstanceRepository;
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-		PortalUtil.setPortalContextPath(config.getServletContext().getContextPath());
-		try {
-			autowire();
-			dbInstallService.install(config.getServletContext());
-		} catch (RuntimeException | DbInstallFailedException e) {
-			log.error(e.getMessage(), e);
-		}
-	}
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        try {
+            autowire();
+            dbInstallService.install(config.getServletContext());
+        } catch (RuntimeException | DbInstallFailedException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PageDisplay pageDisplay = RequestUtil.getPageDisplay(request);
-		RequestInfo.PortletURL portletURL = pageDisplay.getRequestInfo().getPortletURL();
-		log.debug("portletURL = {}", portletURL);
-		if (portletURL != null) {
-			try {
-				if (processPortletRequest(request, response, pageDisplay, portletURL)) {
-					return;
-				}
-			} catch (PrincipalException e) {
-				log.debug(e.getMessage(), e);
-			}
-		}
-		getAndIncludeTheme(request, response, pageDisplay);
-	}
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PageDisplay pageDisplay = RequestUtil.getPageDisplay(request);
+        RequestInfo.PortletURL portletURL = pageDisplay.getRequestInfo().getPortletURL();
+        log.debug("portletURL = {}", portletURL);
+        if (portletURL != null) {
+            try {
+                if (processPortletRequest(request, response, pageDisplay, portletURL)) {
+                    return;
+                }
+            } catch (PrincipalException e) {
+                log.debug(e.getMessage(), e);
+            }
+        }
+        getAndIncludeTheme(request, response, pageDisplay);
+    }
 
-	private boolean processPortletRequest(HttpServletRequest request, HttpServletResponse response, PageDisplay pageDisplay, RequestInfo.PortletURL portletURL)
-			throws PrincipalException {
-		if (PortletLifecycle.ACTION.equals(portletURL.getLifecycle())) {
-			PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
-			if (portletApp != null) {
-				boolean success = portletApp.processAction(request, response);
-				if (response.isCommitted()) {
-					return true;
-				}
-				if (success) {
-					if (MidaiganesWindowState.EXCLUSIVE.equals(portletURL.getWindowState())) {
-						portletApp.doRender(request, response);
-						return true;
-					}
-				} else {
+    private boolean processPortletRequest(HttpServletRequest request, HttpServletResponse response, PageDisplay pageDisplay, RequestInfo.PortletURL portletURL)
+            throws PrincipalException {
+        if (PortletLifecycle.ACTION.equals(portletURL.getLifecycle())) {
+            PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
+            if (portletApp != null) {
+                boolean success = portletApp.processAction(request, response);
+                if (response.isCommitted()) {
+                    return true;
+                }
+                if (success) {
+                    if (MidaiganesWindowState.EXCLUSIVE.equals(portletURL.getWindowState())) {
+                        portletApp.doRender(request, response);
+                        return true;
+                    }
+                } else {
 
-					// TODO
-					log.error("action failed");
-				}
-			}
-		} else if (PortletLifecycle.RESOURCE.equals(portletURL.getLifecycle())) {
-			PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
-			if (portletApp != null) {
-				log.debug("Found PortletApp '{}'", portletApp);
-				portletApp.serveResource(request, response);
-				return true;
-			} else {
-				log.debug("Didn't find portletApp for portletUrl '{}'", portletURL);
-			}
-		} else if (MidaiganesWindowState.EXCLUSIVE.equals(portletURL.getWindowState())) {
-			PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
-			log.debug("portletApp = {}", portletApp);
-			if (portletApp != null) {
-				portletApp.doRender(request, response);
-				return true;
-			}
-		}
-		return false;
-	}
+                    // TODO
+                    log.error("action failed");
+                }
+            }
+        } else if (PortletLifecycle.RESOURCE.equals(portletURL.getLifecycle())) {
+            PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
+            if (portletApp != null) {
+                log.debug("Found PortletApp '{}'", portletApp);
+                portletApp.serveResource(request, response);
+                return true;
+            } else {
+                log.debug("Didn't find portletApp for portletUrl '{}'", portletURL);
+            }
+        } else if (MidaiganesWindowState.EXCLUSIVE.equals(portletURL.getWindowState())) {
+            PortletApp portletApp = getPortletApp(pageDisplay, portletURL);
+            log.debug("portletApp = {}", portletApp);
+            if (portletApp != null) {
+                portletApp.doRender(request, response);
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private PortletApp getPortletApp(PageDisplay pageDisplay, RequestInfo.PortletURL portletURL) throws PrincipalException {
-		// TODO
-		if (StringPool.DEFAULT_PORTLET_WINDOWID.equals(portletURL.getWindowID())) {
-			if (portletURL.getPortletName() != null) {
-				PortletInstance instance = portletInstanceRepository.getDefaultPortletInstance(portletURL.getPortletName());
-				return portletRepository.getPortletApp(pageDisplay.getUser().getId(), instance, portletURL.getPortletMode(), portletURL.getWindowState());
-			} else {
-				log.debug("portletURL.getPortletName is empty");
-			}
-		} else {
-			LayoutPortlet layoutPortlet = getLayoutPortlet(pageDisplay, portletURL);
-			if (layoutPortlet != null) {
-				return portletRepository.getPortletApp(pageDisplay.getUser().getId(), layoutPortlet, portletURL.getPortletMode(), portletURL.getWindowState());
-			} else {
-				log.debug("layoutPortlet = null");
-			}
-		}
-		return null;
-	}
+    private PortletApp getPortletApp(PageDisplay pageDisplay, RequestInfo.PortletURL portletURL) throws PrincipalException {
+        // TODO
+        if (StringPool.DEFAULT_PORTLET_WINDOWID.equals(portletURL.getWindowID())) {
+            if (portletURL.getPortletName() != null) {
+                PortletInstance instance = portletInstanceRepository.getDefaultPortletInstance(portletURL.getPortletName());
+                return portletRepository.getPortletApp(pageDisplay.getUser().getId(), instance, portletURL.getPortletMode(), portletURL.getWindowState());
+            } else {
+                log.debug("portletURL.getPortletName is empty");
+            }
+        } else {
+            LayoutPortlet layoutPortlet = getLayoutPortlet(pageDisplay, portletURL);
+            if (layoutPortlet != null) {
+                return portletRepository.getPortletApp(pageDisplay.getUser().getId(), layoutPortlet, portletURL.getPortletMode(), portletURL.getWindowState());
+            } else {
+                log.debug("layoutPortlet = null");
+            }
+        }
+        return null;
+    }
 
-	private LayoutPortlet getLayoutPortlet(PageDisplay pageDisplay, RequestInfo.PortletURL portletURL) {
-		return layoutPortletRepository.getLayoutPortlet(pageDisplay.getLayout().getId(), portletURL.getWindowID());
-	}
+    private LayoutPortlet getLayoutPortlet(PageDisplay pageDisplay, RequestInfo.PortletURL portletURL) {
+        return layoutPortletRepository.getLayoutPortlet(pageDisplay.getLayout().getId(), portletURL.getWindowID());
+    }
 
-	private void getAndIncludeTheme(HttpServletRequest request, HttpServletResponse response, PageDisplay pageDisplay) throws ServletException, IOException {
-		Theme theme = pageDisplay.getTheme();
-		if (theme != null) {
-			includeTheme(request, response, theme);
-		} else {
-			log.error("theme is null");
-		}
-	}
+    private void getAndIncludeTheme(HttpServletRequest request, HttpServletResponse response, PageDisplay pageDisplay) throws ServletException, IOException {
+        Theme theme = pageDisplay.getTheme();
+        if (theme != null) {
+            includeTheme(request, response, theme);
+        } else {
+            log.error("theme is null");
+        }
+    }
 
-	private void includeTheme(HttpServletRequest request, HttpServletResponse response, Theme theme) throws ServletException, IOException {
-		ServletContext servletContext = ContextUtil.getServletContext(request, theme.getThemeName().getContextWithSlash());
-		if (servletContext == null) {
-			log.error("servletContext = null; theme = " + theme);
-		} else {
-			includeTheme(request, response, theme, servletContext.getRequestDispatcher(WEBINF_THEME));
-		}
-	}
+    private void includeTheme(HttpServletRequest request, HttpServletResponse response, Theme theme) throws ServletException, IOException {
+        ServletContext servletContext = ContextUtil.getServletContext(request, theme.getThemeName().getContextWithSlash());
+        if (servletContext == null) {
+            log.error("servletContext = null; theme = " + theme);
+        } else {
+            includeTheme(request, response, theme, servletContext.getRequestDispatcher(WEBINF_THEME));
+        }
+    }
 
-	private void includeTheme(HttpServletRequest request, HttpServletResponse response, Theme theme, RequestDispatcher requestDispatcher)
-			throws ServletException, IOException {
-		if (requestDispatcher == null) {
-			log.error(ThemeServlet.class.getName() + " not registered");
-		} else {
-			requestDispatcher.include(new ThemeServletRequest(theme, request), response);
-		}
-	}
+    private void includeTheme(HttpServletRequest request, HttpServletResponse response, Theme theme, RequestDispatcher requestDispatcher) throws ServletException, IOException {
+        if (requestDispatcher == null) {
+            log.error(ThemeServlet.class.getName() + " not registered");
+        } else {
+            requestDispatcher.include(new ThemeServletRequest(theme, request), response);
+        }
+    }
 }
--- portal-service/src/main/java/ee/midaiganes/util/PortalUtil.java
+++ portal-service/src/main/java/ee/midaiganes/util/PortalUtil.java
@@ -1,29 +1,8 @@
 package ee.midaiganes.util;
 
 public class PortalUtil {
-	private static String portalContextPath;
 
-	/**
-	 * @deprecated use {@link PropsValues}
-	 */
-	@Deprecated
-	public static String getPortalContextPath() {
-		return portalContextPath;
-	}
-
-	/**
-	 * The path starts with a / character but does not end with a / character.
-	 * For servlets in the default (root) context, this method returns "".
-	 */
-	@Deprecated
-	public synchronized static void setPortalContextPath(String portalContextPath) {
-		if (PortalUtil.portalContextPath != null) {
-			throw new IllegalStateException("Portal context path(" + PortalUtil.portalContextPath + ") is already set.");
-		}
-		PortalUtil.portalContextPath = portalContextPath;
-	}
-
-	public static boolean isSuperAdminUser(long userId) {
-		return ArrayUtil.contains(PropsValues.SUPERADMIN_USER_IDS, userId);
-	}
+    public static boolean isSuperAdminUser(long userId) {
+        return ArrayUtil.contains(PropsValues.SUPERADMIN_USER_IDS, userId);
+    }
 }
