efc2887849c6461801aff103a8f803787a2bd845
midaiganes <midaiganes@panic>
2014-02-12 21:54:47 +0200

    refactor

--- portal-service/src/main/java/ee/midaiganes/util/CollectionUtil.java
+++ portal-service/src/main/java/ee/midaiganes/util/CollectionUtil.java
@@ -4,11 +4,11 @@ import java.util.Collection;
 import java.util.List;
 
 public class CollectionUtil {
-	public static <A> A getFirstElement(List<A> list) {
-		return list != null && !list.isEmpty() ? list.get(0) : null;
-	}
+    public static <A> A getFirstElement(List<A> list) {
+        return list != null && !list.isEmpty() ? list.get(0) : null;
+    }
 
-	public static <A> A getFirstElement(Collection<A> list) {
-		return list != null && !list.isEmpty() ? list.iterator().next() : null;
-	}
+    public static <A> A getFirstElement(Collection<A> list) {
+        return list != null && !list.isEmpty() ? list.iterator().next() : null;
+    }
 }
