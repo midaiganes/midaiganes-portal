b0414c11a5127848cbb627e67ced74dd91b62d85
midaiganes <midaiganes@panic>
2014-03-08 10:21:15 +0200

    fix warning

--- portal-service/src/main/java/ee/midaiganes/servlet/http/GZIPResponse.java
+++ portal-service/src/main/java/ee/midaiganes/servlet/http/GZIPResponse.java
@@ -15,88 +15,88 @@ import org.slf4j.LoggerFactory;
 import ee.midaiganes.servlet.io.ServletOutputStreamImpl;
 
 public class GZIPResponse extends HttpServletResponseWrapper {
-	private static final Logger log = LoggerFactory.getLogger(GZIPResponse.class);
-	private boolean outputCalled;
-	private boolean writerCalled;
-	private ServletOutputStream os;
-	private PrintWriter pw;
-	private GZIPOutputStream gos;
-	private boolean sendRedirectCalled;
+    private static final Logger log = LoggerFactory.getLogger(GZIPResponse.class);
+    private boolean outputCalled;
+    private boolean writerCalled;
+    private ServletOutputStream os;
+    private PrintWriter pw;
+    private GZIPOutputStream gos;
+    private boolean sendRedirectCalled;
 
-	public GZIPResponse(HttpServletResponse response) {
-		super(response);
-	}
+    public GZIPResponse(HttpServletResponse response) {
+        super(response);
+    }
 
-	@Override
-	public void sendRedirect(String location) throws IOException {
-		super.sendRedirect(location);
-		sendRedirectCalled = true;
-	}
+    @Override
+    public void sendRedirect(String location) throws IOException {
+        super.sendRedirect(location);
+        sendRedirectCalled = true;
+    }
 
-	@Override
-	public boolean isCommitted() {
-		return sendRedirectCalled || super.isCommitted();
-	}
+    @Override
+    public boolean isCommitted() {
+        return sendRedirectCalled || super.isCommitted();
+    }
 
-	@Override
-	public void flushBuffer() throws IOException {
-		flushBuffer(false);
-	}
+    @Override
+    public void flushBuffer() throws IOException {
+        flushBuffer(false);
+    }
 
-	public void flushBuffer(boolean finish) throws IOException {
-		log.debug("flushBuffer; finish = {}; sendRedirectCalled = {}", finish, sendRedirectCalled);
-		if (!sendRedirectCalled) {
-			if (writerCalled) {
-				pw.flush();
-			}
-			if (gos != null) {
-				if (finish) {
-					gos.finish();
-				} else {
-					gos.flush();
-				}
-			}
-			super.flushBuffer();
-		}
-	}
+    public void flushBuffer(boolean finish) throws IOException {
+        log.debug("flushBuffer; finish = {}; sendRedirectCalled = {}", Boolean.valueOf(finish), Boolean.valueOf(sendRedirectCalled));
+        if (!sendRedirectCalled) {
+            if (writerCalled) {
+                pw.flush();
+            }
+            if (gos != null) {
+                if (finish) {
+                    gos.finish();
+                } else {
+                    gos.flush();
+                }
+            }
+            super.flushBuffer();
+        }
+    }
 
-	@Override
-	public ServletOutputStream getOutputStream() throws IOException {
-		if (writerCalled) {
-			throw new IllegalStateException();
-		}
-		if (!outputCalled) {
-			initOutputStream();
-			outputCalled = true;
-		}
-		return os;
-	}
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        if (writerCalled) {
+            throw new IllegalStateException();
+        }
+        if (!outputCalled) {
+            initOutputStream();
+            outputCalled = true;
+        }
+        return os;
+    }
 
-	@Override
-	public PrintWriter getWriter() throws IOException {
-		if (outputCalled) {
-			throw new IllegalStateException();
-		}
-		if (!writerCalled) {
-			initWriter();
-			writerCalled = true;
-		}
-		return pw;
-	}
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        if (outputCalled) {
+            throw new IllegalStateException();
+        }
+        if (!writerCalled) {
+            initWriter();
+            writerCalled = true;
+        }
+        return pw;
+    }
 
-	private void initOutputStream() throws IOException {
-		initGZIPOutputStream();
-		os = new ServletOutputStreamImpl(gos);
-	}
+    private void initOutputStream() throws IOException {
+        initGZIPOutputStream();
+        os = new ServletOutputStreamImpl(gos);
+    }
 
-	private void initWriter() throws IOException {
-		initGZIPOutputStream();
-		String encoding = super.getCharacterEncoding();
-		log.debug("encoding = '{}'", encoding);
-		pw = new PrintWriter(new OutputStreamWriter(gos, encoding), false);
-	}
+    private void initWriter() throws IOException {
+        initGZIPOutputStream();
+        String encoding = super.getCharacterEncoding();
+        log.debug("encoding = '{}'", encoding);
+        pw = new PrintWriter(new OutputStreamWriter(gos, encoding), false);
+    }
 
-	private void initGZIPOutputStream() throws IOException {
-		gos = new GZIPOutputStream(super.getOutputStream(), 1024 * 1024);
-	}
+    private void initGZIPOutputStream() throws IOException {
+        gos = new GZIPOutputStream(super.getOutputStream(), 1024 * 1024);
+    }
 }
