74a51b63ea777ed1f6434b0754c3eaa1fe696d10
midaiganes <midaiganes@panic>
2014-10-25 16:18:09 +0300

    replace spring with guice

--- portal-service/src/main/java/ee/midaiganes/secureservices/SecureLayoutRepository.java
+++ portal-service/src/main/java/ee/midaiganes/secureservices/SecureLayoutRepository.java
@@ -3,11 +3,14 @@ package ee.midaiganes.secureservices;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import ee.midaiganes.beans.PortalConfig;
+import ee.midaiganes.beans.PortalBeans;
 import ee.midaiganes.portal.layout.Layout;
 import ee.midaiganes.portal.layout.LayoutRepository;
 import ee.midaiganes.portal.permission.PermissionRepository;
@@ -15,59 +18,61 @@ import ee.midaiganes.services.exceptions.PrincipalException;
 import ee.midaiganes.services.exceptions.ResourceActionNotFoundException;
 import ee.midaiganes.services.exceptions.ResourceNotFoundException;
 
-@Component(value = PortalConfig.SECURE_LAYOUT_REPOSITORY)
+@Component(value = PortalBeans.SECURE_LAYOUT_REPOSITORY)
 public class SecureLayoutRepository {
-	private static final Logger log = LoggerFactory.getLogger(SecureLayoutRepository.class);
-	private static final String VIEW = "VIEW";
-	private final LayoutRepository layoutRepository;
-	private final PermissionRepository permissionRepository;
+    private static final Logger log = LoggerFactory.getLogger(SecureLayoutRepository.class);
+    private static final String VIEW = "VIEW";
+    private final LayoutRepository layoutRepository;
+    private final PermissionRepository permissionRepository;
 
-	public SecureLayoutRepository(LayoutRepository layoutRepository, PermissionRepository permissionRepository) {
-		this.layoutRepository = layoutRepository;
-		this.permissionRepository = permissionRepository;
-	}
+    @Inject
+    public SecureLayoutRepository(LayoutRepository layoutRepository, PermissionRepository permissionRepository) {
+        this.layoutRepository = layoutRepository;
+        this.permissionRepository = permissionRepository;
+    }
 
-	public List<Layout> getLayouts(long userId, long layoutSetId) {
-		List<Layout> layouts = layoutRepository.getLayouts(layoutSetId);
-		List<Layout> allowedLayouts = new ArrayList<>(layouts.size());
-		try {
-			for (Layout layout : layouts) {
-				if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
-					allowedLayouts.add(layout);
-				}
-			}
-		} catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
-			throw new RuntimeException(e);
-		}
+    public List<Layout> getLayouts(long userId, long layoutSetId) {
+        List<Layout> layouts = layoutRepository.getLayouts(layoutSetId);
+        List<Layout> allowedLayouts = new ArrayList<>(layouts.size());
+        try {
+            for (Layout layout : layouts) {
+                if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
+                    allowedLayouts.add(layout);
+                }
+            }
+        } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
-		return allowedLayouts;
-	}
+        return allowedLayouts;
+    }
 
-	public Layout getLayout(long userId, long layoutSetId, String friendlyUrl) throws PrincipalException {
-		Layout layout = layoutRepository.getLayout(layoutSetId, friendlyUrl);
-		try {
-			if (layout == null) {
-				return null;
-			}
-			if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
-				return layout;
-			}
-		} catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		throw new PrincipalException(userId, layout, VIEW);
-	}
+    public Layout getLayout(long userId, long layoutSetId, String friendlyUrl) throws PrincipalException {
+        Layout layout = layoutRepository.getLayout(layoutSetId, friendlyUrl);
+        try {
+            if (layout == null) {
+                return null;
+            }
+            if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
+                return layout;
+            }
+        } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        throw new PrincipalException(userId, layout, VIEW);
+    }
 
-	public Layout getHomeLayout(long userId, long layoutSetId) {
-		for (Layout layout : layoutRepository.getChildLayouts(layoutSetId, null)) {
-			try {
-				if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
-					return layout;
-				}
-			} catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
-				log.debug(e.getMessage(), e);
-			}
-		}
-		return null;
-	}
+    @Nullable
+    public Layout getHomeLayout(long userId, long layoutSetId) {
+        for (Layout layout : layoutRepository.getChildLayouts(layoutSetId, null)) {
+            try {
+                if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
+                    return layout;
+                }
+            } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
+                log.debug(e.getMessage(), e);
+            }
+        }
+        return null;
+    }
 }
--- portal-service/src/main/java/ee/midaiganes/secureservices/SecurePortletRepository.java
+++ portal-service/src/main/java/ee/midaiganes/secureservices/SecurePortletRepository.java
@@ -2,12 +2,13 @@ package ee.midaiganes.secureservices;
 
 import java.util.Locale;
 
+import javax.inject.Inject;
 import javax.portlet.PortletMode;
 import javax.portlet.WindowState;
 
 import org.springframework.stereotype.Component;
 
-import ee.midaiganes.beans.PortalConfig;
+import ee.midaiganes.beans.PortalBeans;
 import ee.midaiganes.portal.layoutportlet.LayoutPortlet;
 import ee.midaiganes.portal.permission.PermissionRepository;
 import ee.midaiganes.portal.portletinstance.PortletInstance;
@@ -17,31 +18,32 @@ import ee.midaiganes.services.exceptions.PrincipalException;
 import ee.midaiganes.services.exceptions.ResourceActionNotFoundException;
 import ee.midaiganes.services.exceptions.ResourceNotFoundException;
 
-@Component(value = PortalConfig.SECURE_PORTLET_REPOSITORY)
+@Component(value = PortalBeans.SECURE_PORTLET_REPOSITORY)
 public class SecurePortletRepository {
 
-	private final PortletRepository portletRepository;
+    private final PortletRepository portletRepository;
 
-	private final PermissionRepository permissionRepository;
+    private final PermissionRepository permissionRepository;
 
-	public SecurePortletRepository(PortletRepository portletRepository, PermissionRepository permissionRepository) {
-		this.portletRepository = portletRepository;
-		this.permissionRepository = permissionRepository;
-	}
+    @Inject
+    public SecurePortletRepository(PortletRepository portletRepository, PermissionRepository permissionRepository) {
+        this.portletRepository = portletRepository;
+        this.permissionRepository = permissionRepository;
+    }
 
-	public PortletApp getPortletApp(long userId, PortletInstance portletInstance, PortletMode portletMode, WindowState windowState) throws PrincipalException {
-		try {
-			final String action = portletMode.toString().toUpperCase(Locale.US);
-			if (permissionRepository.hasUserPermission(userId, portletInstance, action)) {
-				return portletRepository.getPortletApp(portletInstance, portletMode, windowState);
-			}
-			throw new PrincipalException(userId, portletInstance, action);
-		} catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public PortletApp getPortletApp(long userId, PortletInstance portletInstance, PortletMode portletMode, WindowState windowState) throws PrincipalException {
+        try {
+            final String action = portletMode.toString().toUpperCase(Locale.US);
+            if (permissionRepository.hasUserPermission(userId, portletInstance, action)) {
+                return portletRepository.getPortletApp(portletInstance, portletMode, windowState);
+            }
+            throw new PrincipalException(userId, portletInstance, action);
+        } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public PortletApp getPortletApp(long userId, LayoutPortlet layoutPortlet, PortletMode portletMode, WindowState windowState) throws PrincipalException {
-		return getPortletApp(userId, layoutPortlet.getPortletInstance(), portletMode, windowState);
-	}
+    public PortletApp getPortletApp(long userId, LayoutPortlet layoutPortlet, PortletMode portletMode, WindowState windowState) throws PrincipalException {
+        return getPortletApp(userId, layoutPortlet.getPortletInstance(), portletMode, windowState);
+    }
 }
