45e173fb26537babfc0751be4f299f0adef43c6a
midaiganes <midaiganes@panic>
2014-10-25 16:07:52 +0300

    replace spring with guice

--- portal-service/src/main/java/ee/midaiganes/javax/servlet/PortalPluginListener.java
+++ portal-service/src/main/java/ee/midaiganes/javax/servlet/PortalPluginListener.java
@@ -1,32 +1,37 @@
 package ee.midaiganes.javax.servlet;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.annotation.Resource;
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import ee.midaiganes.beans.PortalConfig;
+import ee.midaiganes.beans.PortalBeans;
 import ee.midaiganes.portal.pagelayout.PageLayoutRepository;
 import ee.midaiganes.portal.theme.ThemeRepository;
 import ee.midaiganes.services.PortletRepository;
+import ee.midaiganes.util.GuiceUtil;
 
 public class PortalPluginListener implements ServletContextListener {
     private static final Logger log = LoggerFactory.getLogger(PortalPluginListener.class);
 
-    @Resource(name = PortalConfig.PORTLET_REPOSITORY)
+    @Inject
+    @Resource(name = PortalBeans.PORTLET_REPOSITORY)
     private PortletRepository portletRepository;
 
-    @Resource(name = PortalConfig.THEME_REPOSITORY)
+    @Inject
+    @Resource(name = PortalBeans.THEME_REPOSITORY)
     private ThemeRepository themeRepository;
 
-    @Resource(name = PortalConfig.PAGE_LAYOUT_REPOSITORY)
+    @Inject
+    @Resource(name = PortalBeans.PAGE_LAYOUT_REPOSITORY)
     private PageLayoutRepository pageLayoutRepository;
 
     @Override
@@ -71,7 +76,7 @@ public class PortalPluginListener implements ServletContextListener {
     }
 
     private void autowire(ServletContext sc) {
-        WebApplicationContextUtils.getRequiredWebApplicationContext(getPortalServletContext(sc)).getAutowireCapableBeanFactory().autowireBean(this);
+        GuiceUtil.getInjector(getPortalServletContext(sc)).injectMembers(this);
     }
 
     private ServletContext getPortalServletContext(ServletContext sc) {
@@ -83,7 +88,9 @@ public class PortalPluginListener implements ServletContextListener {
         try (InputStream themeXmlInputStream = sc.getResourceAsStream("/WEB-INF/midaiganes-theme.xml")) {
             log.debug("midaiganes-theme.xml exists ? {}", Boolean.valueOf(themeXmlInputStream != null));
             if (themeXmlInputStream != null) {
-                themeRepository.registerThemes(sc.getContextPath(), themeXmlInputStream);
+                try (BufferedInputStream bis = new BufferedInputStream(themeXmlInputStream)) {
+                    themeRepository.registerThemes(sc.getContextPath(), bis);
+                }
             }
         } catch (RuntimeException | IOException e) {
             log.error(e.getMessage(), e);
@@ -93,7 +100,9 @@ public class PortalPluginListener implements ServletContextListener {
     private void initPageLayouts(ServletContext sc) {
         try (InputStream pageLayoutInputStream = sc.getResourceAsStream("/WEB-INF/midaiganes-layout.xml")) {
             if (pageLayoutInputStream != null) {
-                pageLayoutRepository.registerPageLayouts(sc.getContextPath(), pageLayoutInputStream);
+                try (BufferedInputStream bis = new BufferedInputStream(pageLayoutInputStream)) {
+                    pageLayoutRepository.registerPageLayouts(sc.getContextPath(), bis);
+                }
             }
         } catch (RuntimeException | IOException e) {
             log.error(e.getMessage(), e);
@@ -104,7 +113,9 @@ public class PortalPluginListener implements ServletContextListener {
         try (InputStream portletInputStream = sc.getResourceAsStream("/WEB-INF/portlet.xml")) {
             log.debug("portlet.xml exists ? {}", Boolean.valueOf(portletInputStream != null));
             if (portletInputStream != null) {
-                portletRepository.registerPortlets(sc, portletInputStream);
+                try (BufferedInputStream bis = new BufferedInputStream(portletInputStream)) {
+                    portletRepository.registerPortlets(sc, bis);
+                }
             }
         } catch (RuntimeException | IOException e) {
             log.error(e.getMessage(), e);
--- portal-service/src/main/java/ee/midaiganes/javax/servlet/PortalServletRequestListener.java
+++ portal-service/src/main/java/ee/midaiganes/javax/servlet/PortalServletRequestListener.java
@@ -3,14 +3,20 @@ package ee.midaiganes.javax.servlet;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PortalServletRequestListener implements ServletRequestListener {
+    private static final Logger log = LoggerFactory.getLogger(PortalServletRequestListener.class);
 
-	@Override
-	public void requestDestroyed(ServletRequestEvent sre) {
-	}
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        log.debug("Request destroyed.");
+    }
 
-	@Override
-	public void requestInitialized(ServletRequestEvent sre) {
-	}
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        log.debug("Request initialized.");
+    }
 
 }
