f75df20d8427aef22491192ee4d69f5702726304
midaiganes <midaiganes@panic>
2014-10-25 16:20:10 +0300

    replace spring with guice

--- portal-service/src/main/java/ee/midaiganes/portlet/MidaiganesPortlet.java
+++ portal-service/src/main/java/ee/midaiganes/portlet/MidaiganesPortlet.java
@@ -25,130 +25,141 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MidaiganesPortlet implements MidaiganesPortletMBean, Portlet {
-	private static final Logger log = LoggerFactory.getLogger(MidaiganesPortlet.class);
-	private final Portlet portlet;
-	private final AtomicLong successRenderCount = new AtomicLong();
-	private final AtomicLong failedRenderCount = new AtomicLong();
-	private final AtomicLong successActionCount = new AtomicLong();
-	private final AtomicLong failedActionCount = new AtomicLong();
-	private int activeRequestCount = 0;
-	private boolean destroy = false;
-	private boolean destroyed = false;
-	private final ObjectName objectName;
+    private static final Logger log = LoggerFactory.getLogger(MidaiganesPortlet.class);
+    private final Portlet portlet;
+    private final AtomicLong successRenderCount = new AtomicLong();
+    private final AtomicLong failedRenderCount = new AtomicLong();
+    private final AtomicLong successActionCount = new AtomicLong();
+    private final AtomicLong failedActionCount = new AtomicLong();
+    private int activeRequestCount = 0;
+    private boolean destroy = false;
+    private boolean destroyed = false;
+    private final ObjectName objectName;
+    private final Object lock = new Object();
 
-	public MidaiganesPortlet(Portlet portlet, PortletName portletName) {
-		this.portlet = portlet;
-		try {
-			objectName = new ObjectName("ee.midaiganes:type=Portlet,name=" + portletName.getFullName());
-			getPlatformMBeanServer().registerMBean(this, objectName);
-		} catch (InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException | MalformedObjectNameException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public MidaiganesPortlet(Portlet portlet, PortletName portletName) {
+        this.portlet = portlet;
+        try {
+            objectName = new ObjectName("ee.midaiganes:type=Portlet,name=" + portletName.getFullName());
+            getPlatformMBeanServer().registerMBean(this, objectName);
+        } catch (InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException | MalformedObjectNameException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	public void init(PortletConfig config) throws PortletException {
-		portlet.init(config);
-	}
+    @Override
+    public void init(PortletConfig config) throws PortletException {
+        portlet.init(config);
+    }
 
-	@Override
-	public synchronized void destroy() {
-		destroy = true;
-		checkDestroy();
-	}
+    @Override
+    public void destroy() {
+        synchronized (lock) {
+            destroy = true;
+            checkDestroy();
+        }
+    }
 
-	private synchronized void checkDestroy() {
-		if (destroy && !destroyed && activeRequestCount == 0) {
-			try {
-				destroyed = true;
-				portlet.destroy();
-			} catch (Throwable t) {
-				log.error(t.getMessage(), t);
-			} finally {
-				unregisterMBean();
-			}
-		}
-	}
+    private void checkDestroy() {
+        synchronized (lock) {
+            if (destroy && !destroyed && activeRequestCount == 0) {
+                try {
+                    destroyed = true;
+                    portlet.destroy();
+                } catch (Throwable t) {
+                    log.error(t.getMessage(), t);
+                } finally {
+                    unregisterMBean();
+                }
+            }
+        }
+    }
 
-	private void unregisterMBean() {
-		try {
-			getPlatformMBeanServer().unregisterMBean(objectName);
-		} catch (Throwable e) {
-			log.error(e.getMessage(), e);
-		}
-	}
+    private void unregisterMBean() {
+        try {
+            getPlatformMBeanServer().unregisterMBean(objectName);
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 
-	private synchronized void incrementActiveRequestCount() {
-		if (destroyed) {
-			throw new RuntimeException("portlet is destroyed");
-		}
-		activeRequestCount++;
-	}
+    private void incrementActiveRequestCount() {
+        synchronized (lock) {
+            if (destroyed) {
+                throw new RuntimeException("portlet is destroyed");
+            }
+            activeRequestCount++;
+        }
+    }
 
-	private synchronized void decrementActiveRequestCountAndCheckDestroy() {
-		activeRequestCount--;
-		checkDestroy();
-	}
+    private void decrementActiveRequestCountAndCheckDestroy() {
+        synchronized (lock) {
+            activeRequestCount--;
+            checkDestroy();
+        }
+    }
 
-	public boolean isResourceServingPortlet() {
-		return portlet instanceof ResourceServingPortlet;
-	}
+    public boolean isResourceServingPortlet() {
+        return portlet instanceof ResourceServingPortlet;
+    }
 
-	public void serveResource(ResourceRequest request, ResourceResponse response) throws PortletException, IOException {
-		// TODO
-		((ResourceServingPortlet) portlet).serveResource(request, response);
-	}
+    public void serveResource(ResourceRequest request, ResourceResponse response) throws PortletException, IOException {
+        // TODO
+        ((ResourceServingPortlet) portlet).serveResource(request, response);
+    }
 
-	@Override
-	public void render(RenderRequest request, RenderResponse response) throws PortletException, IOException {
-		try {
-			incrementActiveRequestCount();
-			portlet.render(request, response);
-			successRenderCount.incrementAndGet();
-		} catch (PortletException | IOException | RuntimeException e) {
-			failedRenderCount.incrementAndGet();
-			throw e;
-		} finally {
-			decrementActiveRequestCountAndCheckDestroy();
-		}
-	}
+    @Override
+    public void render(RenderRequest request, RenderResponse response) throws PortletException, IOException {
+        try {
+            incrementActiveRequestCount();
+            portlet.render(request, response);
+            successRenderCount.incrementAndGet();
+        } catch (PortletException | IOException | RuntimeException e) {
+            failedRenderCount.incrementAndGet();
+            throw e;
+        } finally {
+            decrementActiveRequestCountAndCheckDestroy();
+        }
+    }
 
-	@Override
-	public void processAction(ActionRequest request, ActionResponse response) throws PortletException, IOException {
-		try {
-			incrementActiveRequestCount();
-			portlet.processAction(request, response);
-			successActionCount.incrementAndGet();
-		} catch (PortletException | IOException | RuntimeException e) {
-			failedActionCount.incrementAndGet();
-			throw e;
-		} finally {
-			decrementActiveRequestCountAndCheckDestroy();
-		}
-	}
+    @Override
+    public void processAction(ActionRequest request, ActionResponse response) throws PortletException, IOException {
+        try {
+            incrementActiveRequestCount();
+            portlet.processAction(request, response);
+            successActionCount.incrementAndGet();
+        } catch (PortletException | IOException | RuntimeException e) {
+            failedActionCount.incrementAndGet();
+            throw e;
+        } finally {
+            decrementActiveRequestCountAndCheckDestroy();
+        }
+    }
 
-	@Override
-	public long getSuccessRenderCount() {
-		return successRenderCount.get();
-	}
+    @Override
+    public long getSuccessRenderCount() {
+        return successRenderCount.get();
+    }
 
-	@Override
-	public long getFailedRenderCount() {
-		return failedRenderCount.get();
-	}
+    @Override
+    public long getFailedRenderCount() {
+        return failedRenderCount.get();
+    }
 
-	@Override
-	public long getSuccessActionCount() {
-		return successActionCount.get();
-	}
+    @Override
+    public long getSuccessActionCount() {
+        return successActionCount.get();
+    }
 
-	@Override
-	public long getFailedActionCount() {
-		return failedActionCount.get();
-	}
+    @Override
+    public long getFailedActionCount() {
+        return failedActionCount.get();
+    }
 
-	@Override
-	public synchronized int getActiveRequestCount() {
-		return activeRequestCount;
-	}
+    @Override
+    public int getActiveRequestCount() {
+        synchronized (lock) {
+            return activeRequestCount;
+        }
+    }
 }
