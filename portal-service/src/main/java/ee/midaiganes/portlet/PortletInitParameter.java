a3b5c8a0d7e1e9c6e21a36fbec558574087095fd
midaiganes <midaiganes@panic>
2015-03-26 22:50:54 +0200

    refactor

--- portal-service/src/main/java/ee/midaiganes/portlet/PortletInitParameter.java
+++ portal-service/src/main/java/ee/midaiganes/portlet/PortletInitParameter.java
@@ -1,55 +1,56 @@
 package ee.midaiganes.portlet;
 
 import java.io.Serializable;
-import java.util.Collections;
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 public class PortletInitParameter implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private final String id;
-	private final String name;
-	private final String value;
-	private final List<Description> description;
+    private static final long serialVersionUID = 1L;
+    private final String id;
+    private final String name;
+    private final String value;
+    private final ImmutableList<Description> description;
 
-	public PortletInitParameter(String id, String name, String value, List<Description> description) {
-		this.id = id;
-		this.name = name;
-		this.value = value;
-		this.description = description == null ? null : Collections.unmodifiableList(description);
-	}
+    public PortletInitParameter(String id, String name, String value, ImmutableList<Description> description) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.description = description;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getValue() {
-		return value;
-	}
+    public String getValue() {
+        return value;
+    }
 
-	public List<Description> getDescription() {
-		return description;
-	}
+    public List<Description> getDescription() {
+        return description;
+    }
 
-	public static class Description implements Serializable {
-		private static final long serialVersionUID = 1L;
-		private final String lang;
-		private final String value;
+    public static class Description implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private final String lang;
+        private final String value;
 
-		public Description(String lang, String value) {
-			this.lang = lang;
-			this.value = value;
-		}
+        public Description(String lang, String value) {
+            this.lang = lang;
+            this.value = value;
+        }
 
-		public String getLang() {
-			return lang;
-		}
+        public String getLang() {
+            return lang;
+        }
 
-		public String getValue() {
-			return value;
-		}
-	}
+        public String getValue() {
+            return value;
+        }
+    }
 }
