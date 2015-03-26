bdc0dfd9b9262d4ad2a72ce530601b669b53f8ff
midaiganes <midaiganes@panic>
2015-03-26 22:46:20 +0200

    refactor

--- portal-service/src/main/java/ee/midaiganes/secureservices/SecureLayoutRepository.java
+++ portal-service/src/main/java/ee/midaiganes/secureservices/SecureLayoutRepository.java
@@ -1,13 +1,12 @@
 package ee.midaiganes.secureservices;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
 
 import ee.midaiganes.portal.layout.Layout;
 import ee.midaiganes.portal.layout.LayoutRepository;
@@ -28,20 +27,25 @@ public class SecureLayoutRepository {
         this.permissionRepository = permissionRepository;
     }
 
-    public List<Layout> getLayouts(long userId, long layoutSetId) {
-        List<Layout> layouts = layoutRepository.getLayouts(layoutSetId);
-        List<Layout> allowedLayouts = new ArrayList<>(layouts.size());
+    public ImmutableList<Layout> getLayouts(long userId, long layoutSetId) {
+        ImmutableList<Layout> layouts = layoutRepository.getLayouts(layoutSetId);
+        ImmutableList.Builder<Layout> allowedLayouts = ImmutableList.builder();
         try {
             for (Layout layout : layouts) {
-                if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
-                    allowedLayouts.add(layout);
-                }
+                addToAllowedLayoutsIfUserHasViewPermission(userId, layout, allowedLayouts);
             }
         } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        return allowedLayouts;
+        return allowedLayouts.build();
+    }
+
+    private void addToAllowedLayoutsIfUserHasViewPermission(long userId, Layout layout, ImmutableList.Builder<Layout> allowedLayouts) throws ResourceNotFoundException,
+            ResourceActionNotFoundException {
+        if (hasUserViewPermission(userId, layout)) {
+            allowedLayouts.add(layout);
+        }
     }
 
     public Layout getLayout(long userId, long layoutSetId, String friendlyUrl) throws PrincipalException {
@@ -50,7 +54,7 @@ public class SecureLayoutRepository {
             if (layout == null) {
                 return null;
             }
-            if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
+            if (hasUserViewPermission(userId, layout)) {
                 return layout;
             }
         } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
@@ -63,7 +67,7 @@ public class SecureLayoutRepository {
     public Layout getHomeLayout(long userId, long layoutSetId) {
         for (Layout layout : layoutRepository.getChildLayouts(layoutSetId, null)) {
             try {
-                if (permissionRepository.hasUserPermission(userId, layout, VIEW)) {
+                if (hasUserViewPermission(userId, layout)) {
                     return layout;
                 }
             } catch (ResourceNotFoundException | ResourceActionNotFoundException e) {
@@ -71,5 +75,9 @@ public class SecureLayoutRepository {
             }
         }
         return null;
+    }
+
+    private boolean hasUserViewPermission(long userId, Layout layout) throws ResourceNotFoundException, ResourceActionNotFoundException {
+        return permissionRepository.hasUserPermission(userId, layout, VIEW);
     }
 }
