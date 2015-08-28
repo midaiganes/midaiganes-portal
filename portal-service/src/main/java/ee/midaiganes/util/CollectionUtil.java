df27868e516639fe25080ce09fe0715f1706502f
midaiganes <midaiganes@panic>
2015-08-28 23:55:34 +0300

    refactor

--- portal-service/src/main/java/ee/midaiganes/util/CollectionUtil.java
+++ portal-service/src/main/java/ee/midaiganes/util/CollectionUtil.java
@@ -1,18 +1,12 @@
 package ee.midaiganes.util;
 
-import java.util.Collection;
 import java.util.List;
 
 import javax.annotation.Nullable;
 
 public class CollectionUtil {
     @Nullable
-    public static <A> A getFirstElement(@Nullable List<A> list) {
+    public static <A> A getFirstElementOrNull(@Nullable List<A> list) {
         return list != null && !list.isEmpty() ? list.get(0) : null;
-    }
-
-    @Nullable
-    public static <A> A getFirstElement(@Nullable Collection<A> list) {
-        return list != null && !list.isEmpty() ? list.iterator().next() : null;
     }
 }
