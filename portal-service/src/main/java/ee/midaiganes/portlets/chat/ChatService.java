c7318a9c769099f1f0ed00482a29f4a0cc5d5db8
midaiganes <midaiganes@panic>
2015-08-28 23:52:49 +0300

    refactor

--- portal-service/src/main/java/ee/midaiganes/portlets/chat/ChatService.java
+++ portal-service/src/main/java/ee/midaiganes/portlets/chat/ChatService.java
@@ -2,6 +2,8 @@ package ee.midaiganes.portlets.chat;
 
 import java.util.List;
 
+import com.google.common.base.Preconditions;
+
 import ee.midaiganes.portal.user.User;
 import ee.midaiganes.portlets.chat.Chat.AddUserToChatResponse;
 import ee.midaiganes.portlets.chat.Chat.SendAndRemoveUserChatMessages;
@@ -10,37 +12,38 @@ import ee.midaiganes.portlets.chat.Chat.SendMessageToChat;
 import ee.midaiganes.portlets.chat.Chat.SendPrivateMessageToUserResponse;
 
 public class ChatService {
-	private final Chats chats = new Chats();
+    private final Chats chats = new Chats();
 
-	public List<ChatModel> getChats() {
-		return chats.getChats();
-	}
+    public List<ChatModel> getChats() {
+        return chats.getChats();
+    }
 
-	public AddUserToChatResponse addUserToChat(User user, long chatId) {
-		return chats.getChat(chatId).addUserToChat(user);
-	}
+    public AddUserToChatResponse addUserToChat(User user, long chatId) {
+        return chats.getChat(chatId).addUserToChat(user);
+    }
 
-	public SendMessageToChat sendMessageToChat(User user, long chatId, String msg) {
-		return chats.getChat(chatId).sendMessageToChat(user, msg);
-	}
+    public SendMessageToChat sendMessageToChat(User user, long chatId, String msg) {
+        return chats.getChat(chatId).sendMessageToChat(user, msg);
+    }
 
-	public SendPrivateMessageToUserResponse sendPrivateMessageToUser(User from, User to, long chatId, String msg) {
-		return chats.getChat(chatId).sendPrivateMessageToUser(from, to, msg);
-	}
+    public SendPrivateMessageToUserResponse sendPrivateMessageToUser(User from, User to, long chatId, String msg) {
+        return chats.getChat(chatId).sendPrivateMessageToUser(from, to, msg);
+    }
 
-	public SendAndRemoveUserChatMessages sendAndRemoveUserChatMessages(long chatId, SendAndRemoveUserChatMessagesRequest call) {
-		return chats.getChat(chatId).sendAndRemoveUserChatMessages(call);
-	}
+    public SendAndRemoveUserChatMessages sendAndRemoveUserChatMessages(long chatId, SendAndRemoveUserChatMessagesRequest call) {
+        return chats.getChat(chatId).sendAndRemoveUserChatMessages(call);
+    }
 
-	public void setUsersPrivateInChat(User user1, User user2, long chatId) {
-		chats.getChat(chatId).setUsersPrivateInChat(user1, user2);
-	}
+    public void setUsersPrivateInChat(User user1, User user2, long chatId) {
+        chats.getChat(chatId).setUsersPrivateInChat(user1, user2);
+    }
 
-	public void setUsersPublicInChat(User user1, long chatId) {
-		chats.getChat(chatId);// TODO
-	}
+    public void setUsersPublicInChat(User user1, long chatId) {
+        Preconditions.checkNotNull(user1);
+        chats.getChat(chatId);// TODO
+    }
 
-	public void destroyAll() {
-		chats.destroyAll();
-	}
+    public void destroyAll() {
+        chats.destroyAll();
+    }
 }
