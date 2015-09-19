f5595bca52f0211b55a7a5c646d44ba81d1600d8
midaiganes <midaiganes@panic>
2015-09-19 23:01:51 +0300

    Make NavItem immutable

--- portal-service/src/main/java/ee/midaiganes/model/NavItem.java
+++ portal-service/src/main/java/ee/midaiganes/model/NavItem.java
@@ -1,14 +1,13 @@
 package ee.midaiganes.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Ordering;
 
 import ee.midaiganes.portal.layout.Layout;
 import ee.midaiganes.util.PortalURLUtil;
@@ -19,22 +18,23 @@ public class NavItem implements Serializable, Comparable<NavItem> {
     private final ImmutableList<NavItem> childs;
     private final long languageId;
 
-    public NavItem(Layout layout, @Nonnull List<Layout> layouts, long languageId) {
+    public NavItem(Layout layout, @Nonnull ImmutableList<Layout> layouts, long languageId) {
         Preconditions.checkNotNull(layouts, "Layouts is null");
         this.languageId = languageId;
         this.layout = Preconditions.checkNotNull(layout, "Layout is null");
-        List<NavItem> childs = new ArrayList<>();
-        for (Layout l : layouts) {
-            Long parentId = l.getParentId();
-            if (parentId != null && parentId.longValue() == this.layout.getId()) {
-                childs.add(new NavItem(l, layouts, languageId));
-            }
-        }
-        Collections.sort(childs);
-        this.childs = ImmutableList.copyOf(childs);
+        this.childs = findChilds(layout.getId(), layouts, languageId);
     }
 
-    public List<NavItem> getChilds() {
+    private static ImmutableList<NavItem> findChilds(long layoutId, ImmutableList<Layout> layouts, long languageId) {
+        Iterable<Layout> filter = Iterables.filter(layouts, l -> {
+            Long parentId = l.getParentId();
+            return parentId != null && parentId.longValue() == layoutId;
+        });
+        Iterable<NavItem> transform = Iterables.transform(filter, l -> new NavItem(l, layouts, languageId));
+        return Ordering.natural().immutableSortedCopy(transform);
+    }
+
+    public ImmutableList<NavItem> getChilds() {
         return childs;
     }
 
