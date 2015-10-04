a9fe366f6d199f7c83e5f5a2f4ded86fbfe27126
midaiganes <midaiganes@panic>
2015-10-04 21:55:21 +0300

    refactor

--- portal-service/src/main/java/ee/midaiganes/portal/permission/ResourceActionRepository.java
+++ portal-service/src/main/java/ee/midaiganes/portal/permission/ResourceActionRepository.java
@@ -1,7 +1,5 @@
 package ee.midaiganes.portal.permission;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
@@ -10,6 +8,7 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 
 import ee.midaiganes.services.exceptions.ResourceActionNotFoundException;
 import ee.midaiganes.services.exceptions.ResourceHasNoActionsException;
@@ -19,16 +18,24 @@ public class ResourceActionRepository {
 
     @Inject
     public ResourceActionRepository(ResourceActionDao resourceActionDao) {
-        this.cache = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS).build(new CacheLoader<Long, ImmutableList<ResourceActionPermission>>() {
-            @Override
-            public ImmutableList<ResourceActionPermission> load(Long resourceId) throws Exception {
-                return ImmutableList.copyOf(resourceActionDao.loadResourceActionPermissions(resourceId.longValue()));
-            }
-        });
+        this.cache = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS).build(new ResourceActionCacheLoader(resourceActionDao));
+    }
+
+    private static class ResourceActionCacheLoader extends CacheLoader<Long, ImmutableList<ResourceActionPermission>> {
+        private final ResourceActionDao resourceActionDao;
+
+        public ResourceActionCacheLoader(ResourceActionDao resourceActionDao) {
+            this.resourceActionDao = resourceActionDao;
+        }
+
+        @Override
+        public ImmutableList<ResourceActionPermission> load(Long resourceId) throws Exception {
+            return ImmutableList.copyOf(resourceActionDao.loadResourceActionPermissions(resourceId.longValue()));
+        }
     }
 
     public long getResourceActionPermission(long resourceId, String action) throws ResourceActionNotFoundException {
-        List<ResourceActionPermission> resourceActionPermissions = cache.getUnchecked(Long.valueOf(resourceId));
+        ImmutableList<ResourceActionPermission> resourceActionPermissions = cache.getUnchecked(Long.valueOf(resourceId));
         if (!resourceActionPermissions.isEmpty()) {
             for (ResourceActionPermission resourceActionPermission : resourceActionPermissions) {
                 if (resourceActionPermission.getResourceId() == resourceId && resourceActionPermission.getAction().equals(action)) {
@@ -41,11 +48,7 @@ public class ResourceActionRepository {
     }
 
     public ImmutableList<String> getResourceActions(long resourceId) {
-        List<ResourceActionPermission> resourceActionPermissions = cache.getUnchecked(Long.valueOf(resourceId));
-        List<String> actions = new ArrayList<>(resourceActionPermissions.size());
-        for (ResourceActionPermission resourceActionPermission : resourceActionPermissions) {
-            actions.add(resourceActionPermission.getAction());
-        }
-        return ImmutableList.copyOf(actions);
+        ImmutableList<ResourceActionPermission> resourceActionPermissions = cache.getUnchecked(Long.valueOf(resourceId));
+        return ImmutableList.copyOf(Iterables.transform(resourceActionPermissions, r -> r.getAction()));
     }
 }
